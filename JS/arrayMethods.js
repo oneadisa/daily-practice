8242dbc2965685b4dcfef8f53f0e5bae15a91897
Abdul Ibrahim <theoneadisa@gmail.com>
2022-08-07 23:14:22 +0100

    arrays and more arrays[2]

--- JS/arrayMethods.js
+++ JS/arrayMethods.js
@@ -169,3 +169,19 @@ const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
 console.log(shoutGreetings(greetings))
 // Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
 
+// <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+// Write your code here:
+
+function sort (a, b) {
+    return b - a;
+}
+
+const sortYears = (arr, dort) => arr.sort(sort);
+
+// Feel free to uncomment the below code to test your function:
+
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922];
+
+console.log(sortYears(years));
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
