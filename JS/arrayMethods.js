47a65f4cecfbf43907fc33841d7e44318223ab60
Abdul Ibrahim <theoneadisa@gmail.com>
2022-08-07 22:19:25 +0100

    arrays and more arrays[1]

--- JS/arrayMethods.js
+++ JS/arrayMethods.js
@@ -70,3 +70,102 @@ const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
 
 console.log(convertToBabyMap(animals))
 // Should return ['baby panda', 'baby turtle', 'baby giraffe', 'baby hippo', 'baby sloth', 'baby human'];
+
+// <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = arr => {
+    let results = [];
+    // The 'outer' for loop:
+    for (let i = 0; i < arr.length; i++) {
+        number = arr[i];
+
+        // The 'inner' while loop:
+        // We needed to create a new variable!
+        let j = 1;
+        while (j < number) {
+            j = j * 2;
+        }
+        results.push(j);
+    }
+    return results
+}
+
+console.log(smallestPowerOfTwo(numbers)) 
+
+// <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+    console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
+
+// Checkpoint 1 solutions:
+const declineEverything = arr => {
+    arr.forEach(politelyDecline)
+}
+
+/*
+// As a function declaration:
+function declineEverything(arr) {
+      arr.forEach(politelyDecline)
+}
+*/
+
+
+// Checkpoint 2 solutions:
+// Using an anonymous function and string interpolation:
+const acceptEverything = arr => {
+    arr.forEach(e => {
+        console.log(`Ok, I guess I will eat some ${e}.`)
+    })
+}
+
+/*
+// Using a named function and string concatenation:
+const grudginglyAccept = veg => {
+      console.log('Ok, I guess I will eat some ' + veg + '.');
+}
+const acceptEverything = arr => {
+      arr.forEach(grudginglyAccept)
+}
+
+// Using a loop:
+const acceptEverything = arr => {
+ for(let i = 0; i<arr.length; i++){
+    console.log(`Ok, I guess I will eat some ${arr[i]}.`)
+ }
+}
+*/
+
+
+// <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+const numbers1 = [2, 7, 9, 171, 52, 33, 14];
+
+const toSquare = (num) => num * num;
+
+// Write your code here:
+
+const squareNums = (arr) => arr.map(num =>
+    num * num
+);
+;
+console.log(squareNums(numbers1));
+
+// <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+// Write your code here:
+const shoutGreetings = arr =>
+    arr.map((item) => `${item.toUpperCase()}!`)
+
+
+// Feel free to uncomment out the code below to test your function!
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
+// Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
